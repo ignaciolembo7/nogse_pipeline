cb81d80fc3312170e37784c2df3261c21a4adb6b
Ignacio Lembo Ferrari <125519052+ignaciolembo7@users.noreply.github.com>
2026-06-04 11:46:27 +0200

    Add preprocessing package entrypoint

---
+++ src/preprocessing/__init__.py
@@ -0,0 +1,5 @@
+"""Preprocessing utilities for NOGSE/OGSE DWI inputs."""
+
+from .workflow import PreprocessingConfig, build_preprocessing_plan, run_preprocessing
+
+__all__ = ["PreprocessingConfig", "build_preprocessing_plan", "run_preprocessing"]
